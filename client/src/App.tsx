a31d4c06cdb096147933fa05b44a8b2f16a19634
gabullish <gabrielcbfigueiredo@outlook.com>
2026-04-04 22:38:58 +0000

    fix: move QueryClientProvider to main.tsx to fix blank screen crash

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { Switch, Route, Router } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { PerplexityAttribution } from "@/components/PerplexityAttribution";
@@ -318,9 +317,9 @@ export default function App() {
   }, [accessMode, agentSession, onSelectMode, isOnBreak]);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       {appShell}
       <Toaster />
-    </QueryClientProvider>
+    </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { queryClient } from "./lib/queryClient";
 import "./index.css";
 
 if (!window.location.hash) {
   window.location.hash = "#/";
 }
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
